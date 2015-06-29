398f5a5e0c7d970afe9d5ca7fa9f2b0565d633b9
Dmitry Gordeev <netimen@gmail.com>
2015-06-29 14:10:14 +0300

    initial commit

---
+++ app/src/main/java/com/netimen/floatingtoolbar/DemoActivity.java
@@ -0,0 +1,11 @@
+package com.netimen.floatingtoolbar;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.EActivity;
+
+
+@EActivity(R.layout.activity_demo)
+public class DemoActivity extends AppCompatActivity {
+
+}
